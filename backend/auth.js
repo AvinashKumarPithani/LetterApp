03fd94536f67080a74a644a8171ffa27c2f1e32e
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-03-30 15:19:22 +0530

    updated auth.js

--- backend/auth.js
+++ backend/auth.js
@@ -9,7 +9,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback"
+    callbackURL: process.env.GOOGLE_REDIRECT_URI
   },
   (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
