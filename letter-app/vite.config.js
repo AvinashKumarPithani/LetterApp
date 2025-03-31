d1b28de0c7ddaedea246e1d3a66559965fcd3e6c
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-03-31 15:07:11 +0530

    prod error handle17

--- letter-app/vite.config.js
+++ letter-app/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ⬅️ Change this to `/` (very important)
+  base: "/", // 🔹 Keep this as `/` for Vercel
   build: {
     outDir: "dist",
   },
